d8ffa4b5131d1ec7c176b4d1972f77a3f489e453
Uchio KONDO <udzura@pepabo.com>
2017-04-17 17:24:31 +0900

    Simple example for ptrace

---
+++ ptrace-example.c
@@ -0,0 +1,490 @@
+/*
+ * Copyright (c) 2008, Katsuhiro Suzuki.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the Katsuhiro Suzuki nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* original from: http://www.katsuster.net/?arg_date=20080724 (Japanese)
+ * adapted for x86_64
+ */
+
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *name[] = {
+    "read",
+    "write",
+    "open",
+    "close",
+    "stat",
+    "fstat",
+    "lstat",
+    "poll",
+    "lseek",
+    "mmap",
+    "mprotect",
+    "munmap",
+    "brk",
+    "rt_sigaction",
+    "rt_sigprocmask",
+    "rt_sigreturn",
+    "ioctl",
+    "pread",
+    "pwrite",
+    "readv",
+    "writev",
+    "access",
+    "pipe",
+    "select",
+    "sched_yield",
+    "mremap",
+    "msync",
+    "mincore",
+    "madvise",
+    "shmget",
+    "shmat",
+    "shmctl",
+    "dup",
+    "dup2",
+    "pause",
+    "nanosleep",
+    "getitimer",
+    "alarm",
+    "setitimer",
+    "getpid",
+    "sendfile",
+    "socket",
+    "connect",
+    "accept",
+    "sendto",
+    "recvfrom",
+    "sendmsg",
+    "recvmsg",
+    "shutdown",
+    "bind",
+    "listen",
+    "getsockname",
+    "getpeername",
+    "socketpair",
+    "setsockopt",
+    "getsockopt",
+    "clone",
+    "fork",
+    "vfork",
+    "execve",
+    "exit",
+    "wait4",
+    "kill",
+    "uname",
+    "semget",
+    "semop",
+    "semctl",
+    "shmdt",
+    "msgget",
+    "msgsnd",
+    "msgrcv",
+    "msgctl",
+    "fcntl",
+    "flock",
+    "fsync",
+    "fdatasync",
+    "truncate",
+    "ftruncate",
+    "getdents",
+    "getcwd",
+    "chdir",
+    "fchdir",
+    "rename",
+    "mkdir",
+    "rmdir",
+    "creat",
+    "link",
+    "unlink",
+    "symlink",
+    "readlink",
+    "chmod",
+    "fchmod",
+    "chown",
+    "fchown",
+    "lchown",
+    "umask",
+    "gettimeofday",
+    "getrlimit",
+    "getrusage",
+    "sysinfo",
+    "times",
+    "ptrace",
+    "getuid",
+    "syslog",
+    "getgid",
+    "setuid",
+    "setgid",
+    "geteuid",
+    "getegid",
+    "setpgid",
+    "getppid",
+    "getpgrp",
+    "setsid",
+    "setreuid",
+    "setregid",
+    "getgroups",
+    "setgroups",
+    "setresuid",
+    "getresuid",
+    "setresgid",
+    "getresgid",
+    "getpgid",
+    "setfsuid",
+    "setfsgid",
+    "getsid",
+    "capget",
+    "capset",
+    "rt_sigpending",
+    "rt_sigtimedwait",
+    "rt_sigqueueinfo",
+    "rt_sigsuspend",
+    "sigaltstack",
+    "utime",
+    "mknod",
+    "uselib",
+    "personality",
+    "ustat",
+    "statfs",
+    "fstatfs",
+    "sysfs",
+    "getpriority",
+    "setpriority",
+    "sched_setparam",
+    "sched_getparam",
+    "sched_setscheduler",
+    "sched_getscheduler",
+    "sched_get_priority_max",
+    "sched_get_priority_min",
+    "sched_rr_get_interval",
+    "mlock",
+    "munlock",
+    "mlockall",
+    "munlockall",
+    "vhangup",
+    "modify_ldt",
+    "pivot_root",
+    "_sysctl",
+    "prctl",
+    "arch_prctl",
+    "adjtimex",
+    "setrlimit",
+    "chroot",
+    "sync",
+    "acct",
+    "settimeofday",
+    "mount",
+    "umount2",
+    "swapon",
+    "swapoff",
+    "reboot",
+    "sethostname",
+    "setdomainname",
+    "iopl",
+    "ioperm",
+    "create_module",
+    "init_module",
+    "delete_module",
+    "get_kernel_syms",
+    "query_module",
+    "quotactl",
+    "nfsservctl",
+    "getpmsg",
+    "putpmsg",
+    "afs_syscall",
+    "tuxcall",
+    "security",
+    "gettid",
+    "readahead",
+    "setxattr",
+    "lsetxattr",
+    "fsetxattr",
+    "getxattr",
+    "lgetxattr",
+    "fgetxattr",
+    "listxattr",
+    "llistxattr",
+    "flistxattr",
+    "removexattr",
+    "lremovexattr",
+    "fremovexattr",
+    "tkill",
+    "time",
+    "futex",
+    "sched_setaffinity",
+    "sched_getaffinity",
+    "set_thread_area",
+    "io_setup",
+    "io_destroy",
+    "io_getevents",
+    "io_submit",
+    "io_cancel",
+    "get_thread_area",
+    "lookup_dcookie",
+    "epoll_create",
+    "epoll_ctl_old",
+    "epoll_wait_old",
+    "remap_file_pages",
+    "getdents64",
+    "set_tid_address",
+    "restart_syscall",
+    "semtimedop",
+    "fadvise64",
+    "timer_create",
+    "timer_settime",
+    "timer_gettime",
+    "timer_getoverrun",
+    "timer_delete",
+    "clock_settime",
+    "clock_gettime",
+    "clock_getres",
+    "clock_nanosleep",
+    "exit_group",
+    "epoll_wait",
+    "epoll_ctl",
+    "tgkill",
+    "utimes",
+    "vserver",
+    "mbind",
+    "set_mempolicy",
+    "get_mempolicy",
+    "mq_open",
+    "mq_unlink",
+    "mq_timedsend",
+    "mq_timedreceive",
+    "mq_notify",
+    "mq_getsetattr",
+    "kexec_load",
+    "waitid",
+    "add_key",
+    "request_key",
+    "keyctl",
+    "ioprio_set",
+    "ioprio_get",
+    "inotify_init",
+    "inotify_add_watch",
+    "inotify_rm_watch",
+    "migrate_pages",
+    "openat",
+    "mkdirat",
+    "mknodat",
+    "fchownat",
+    "futimesat",
+    "newfstatat",
+    "unlinkat",
+    "renameat",
+    "linkat",
+    "symlinkat",
+    "readlinkat",
+    "fchmodat",
+    "faccessat",
+    "pselect6",
+    "ppoll",
+    "unshare",
+    "set_robust_list",
+    "get_robust_list",
+    "splice",
+    "tee",
+    "sync_file_range",
+    "vmsplice",
+    "move_pages",
+    "utimensat",
+    "epoll_pwait",
+    "signalfd",
+    "timerfd",
+    "eventfd",
+    "fallocate",
+    "timerfd_settime",
+    "timerfd_gettime",
+    "accept4",
+    "signalfd4",
+    "eventfd2",
+    "epoll_create1",
+    "dup3",
+    "pipe2",
+    "inotify_init1",
+    "preadv",
+    "pwritev",
+    "rt_tgsigqueueinfo",
+    "perf_event_open",
+    "recvmmsg",
+    "fanotify_init",
+    "fanotify_mark",
+    "prlimit64",
+    "name_to_handle_at",
+    "open_by_handle_at",
+    "clock_adjtime",
+    "syncfs",
+    "sendmmsg",
+    "setns",
+    "getcpu",
+    "process_vm_readv",
+    "process_vm_writev",
+    "kcmp",
+    "finit_module",
+    "sched_setattr",
+    "sched_getattr",
+    "renameat2",
+    "seccomp",
+    "getrandom",
+    "memfd_create",
+    "kexec_file_load",
+    "bpf",
+    "execveat",
+};
+
+// x86_64 has no socketcall!!1
+/* const char *socketname[] = { */
+/*     "0",          "socket",      "bind",        "connect",    "listen", */
+/*     "accept",     "getsockname", "getpeername", "socketpair", "send", */
+/*     "recv",       "sendto",      "recvfrom",    "shutdown",   "setsockopt",
+ */
+/*     "getsockopt", "sendmsg",     "recvmsg", */
+/* }; */
+
+int func_hook(pid_t pid) {
+  struct user_regs_struct regs;
+  int syscall, socketcall;
+  int result;
+  int i;
+
+  result = ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+  if (result) {
+    fprintf(stderr, "%5d: GETREGS failed\n", pid);
+    return 1;
+  }
+
+  syscall = regs.orig_rax;
+  fprintf(stderr, "%5d: sys%4s(%3d): %s\n", pid,
+          (regs.rax == -ENOSYS) ? "in" : "out", syscall, name[syscall]);
+
+  return 0;
+}
+
+int parent_main(void) {
+  pid_t child;
+  int status, sig;
+  int result;
+
+  while (1) {
+    child = waitpid(-1, &status, WUNTRACED | WCONTINUED);
+    if (child == -1) {
+      perror("waitpid");
+      return 1;
+    }
+
+    sig = 0;
+
+    if (WIFEXITED(status)) {
+      fprintf(stderr, "%5d: exited, st:%d\n", child, WEXITSTATUS(status));
+      break;
+    } else if (WIFSIGNALED(status)) {
+      fprintf(stderr, "%5d: signaled, sig:%d, core:%s\n", child,
+              WTERMSIG(status), (WCOREDUMP(status)) ? "yes" : "no");
+      break;
+    } else if (WIFSTOPPED(status)) {
+      if (WSTOPSIG(status) == SIGTRAP) {
+        // trap
+        result = func_hook(child);
+        if (result) {
+          fprintf(stderr, "%5d: hook failed.\n", child);
+          return 1;
+        }
+      } else {
+        // not trap
+        fprintf(stderr, "%5d: stopped, sig:%d\n", child, WSTOPSIG(status));
+
+        sig = WSTOPSIG(status);
+      }
+    } else if (WIFCONTINUED(status)) {
+      fprintf(stderr, "%5d: continued.\n", child);
+    } else {
+      fprintf(stderr, "%5d: illegal status %d.", child, status);
+      return 1;
+    }
+
+    result = ptrace(PTRACE_SYSCALL, child, NULL, sig);
+    if (result == -1) {
+      perror("ptrace(PTRACE_SYSCALL, ...)");
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int child_main(const char *filename, char *argv[]) {
+  int result;
+
+  ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+
+  result = execvp(filename, argv);
+  if (result) {
+    perror("execvp");
+    return result;
+  }
+
+  printf("[bug] never reached here.\n");
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  const char *filename;
+  pid_t pid;
+  int result;
+
+  if (argc < 2) {
+    printf("usage: \n%s execfile [options]\n", argv[0]);
+    return 0;
+  }
+
+  filename = argv[1];
+
+  pid = fork();
+  if (pid) {
+    // parent
+    fprintf(stderr, "%5d: child started\n", pid);
+
+    result = parent_main();
+  } else {
+    // child
+    result = child_main(filename, &argv[1]);
+  }
+
+  return result;
+}
